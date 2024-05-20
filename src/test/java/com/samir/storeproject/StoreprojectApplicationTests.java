ab872281bdfe33e3ea83d3815fb623667aff7c41
Samir <122364498+Stileexx@users.noreply.github.com>
2024-05-20 23:21:12 +0200

    init Store Project

---
+++ src/test/java/com/samir/storeproject/StoreprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samir.storeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
